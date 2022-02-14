03b999ef76b71beb96f623d27bde82d9852cd54d
vladislav_liboras <liboras.vlad@gmail.com>
2022-02-14 23:25:46 +0300

    cant read in th same time 3 values

--- src/by/jonline/module1/Application_1.java
+++ src/by/jonline/module1/Application_1.java
@@ -1,5 +1,25 @@
 package by.jonline.module1;
 
-public class Application_1 {
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
+public class Application_1 {
+	public static void main(String[] args) {
+		
+		try {
+			BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+			
+			System.out.print("Enter a, b, c: ");
+			Double a = Double.parseDouble(in.readLine());
+			Double b = Double.parseDouble(in.readLine());
+			Double c = Double.parseDouble(in.readLine());
+			
+			Double z = ((a - 3) * b / 2) + c;
+			
+			System.out.println("Z equals: " + z);
+			
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
 }
